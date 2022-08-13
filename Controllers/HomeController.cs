daa79c9076286d2acaa183064aec5ca06ae3f174
MonoT22 <u19078308@tuks.co.za>
2022-08-13 02:13:49 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u19078308HW04.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Our mission and Our focus";
+
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u19078308HW04.MvcApplication" Language="C#" %>
